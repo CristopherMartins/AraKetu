4388e7ab7da091ec4a8723115e5b191c7014493f
Cristopher Martins <cristopherabreu12@gmail.com>
2018-04-19 16:18:58 -0300

    Primeira Calculadora (confimação)

--- calculadora/app/src/main/java/com/cristopher/calculadora/MainActivity.java
+++ calculadora/app/src/main/java/com/cristopher/calculadora/MainActivity.java
@@ -18,18 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
         Button btCalcular = findViewById(R.id.btnCalcular);
 
-        EditText v1 = findViewById(R.id.valor1);
-        Integer valor1 = Integer.valueOf(v1.getText().toString());
-
-        EditText v2 = findViewById(R.id.valor2);
-        Integer valor2 = Integer.valueOf(v2.getText().toString());
-
-        RadioGroup radioGroup = findViewById(R.id.rdOperacao);
-
-        btCalcular.setOnClickListener(calcular(valor1, valor2, radioGroup));
+        btCalcular.setOnClickListener(onClickCalcular());
     }
 
-    /*private View.OnClickListener onClickCalcular(){
+    private View.OnClickListener onClickCalcular(){
         return new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -42,37 +34,36 @@ public class MainActivity extends AppCompatActivity {
                 valor2 = Integer.valueOf(v2.getText().toString());
 
                 RadioGroup op = findViewById(R.id.rdOperacao);
-                //String rdOperacao =((RadioButton)findViewById(op.getCheckedRadioButtonId())).getText().toString();
+                String rdOperacao =((RadioButton)findViewById(op.getCheckedRadioButtonId())).getText().toString();
 
                 Intent intent = new Intent(getContext(), ResultActivity.class);
-                int resultado = calcular(valor1, valor2, op);
+                int resultado = calcular(valor1, valor2, rdOperacao);
                 Bundle params = new Bundle();
                 params.putInt("resultado", resultado);
                 intent.putExtras(params);
                 startActivity(intent);
 
-                //calcular(valor1, valor2, rdOperacao);
             }
         };
-    }*/
+    }
 
-    private void calcular(int valor1, int valor2, RadioGroup operacao){
+    private int calcular(int valor1, int valor2, String operacao){
         int result = 0;
-        switch (operacao.getCheckedRadioButtonId()){
-            case R.id.adicao:
+        switch (operacao){
+            case "+":
                 result = valor1 + valor2;
                 break;
-            case R.id.subtracao:
+            case "-":
                 result = valor1 - valor2;
                 break;
-            case R.id.multiplicacao:
+            case "x":
                 result = valor1 * valor2;
                 break;
-            case R.id.divisao:
+            case "/":
                 result = valor1 / valor2;
                 break;
         }
-        //return result;
+        return result;
     }
 
     private Context getContext() {
